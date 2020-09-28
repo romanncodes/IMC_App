c236b2091c6356ede888e02cd1896722fd92fefd
wladimeer <wladimeerbc@gmail.com>
2020-09-28 17:47:23 -0300

    metodos setter and getter

--- app/src/main/java/cl/roman/imc_app/model/Modelo.java
+++ app/src/main/java/cl/roman/imc_app/model/Modelo.java
@@ -3,10 +3,35 @@ package cl.roman.imc_app.model;
 public class Modelo {
     public String nomnbre;
     public String apellido;
-
+    public int edad;
 
     public Modelo(String nomnbre, String apellido) {
         this.nomnbre = nomnbre;
         this.apellido = apellido;
     }
+
+    public String getNomnbre() {
+        return nomnbre;
+    }
+
+    public void setNomnbre(String nomnbre) {
+        this.nomnbre = nomnbre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
 }
