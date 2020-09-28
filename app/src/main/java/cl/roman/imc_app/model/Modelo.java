9dff8e02b15ec1c02b18ffe4e5e0c9b505f1a860
Roman <roman.gajardo@gmail.com>
2020-09-28 17:41:33 -0300

    modelo agregado con propiedes.....

---
+++ app/src/main/java/cl/roman/imc_app/model/Modelo.java
@@ -0,0 +1,12 @@
+package cl.roman.imc_app.model;
+
+public class Modelo {
+    public String nomnbre;
+    public String apellido;
+
+
+    public Modelo(String nomnbre, String apellido) {
+        this.nomnbre = nomnbre;
+        this.apellido = apellido;
+    }
+}
